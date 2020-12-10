0fefe0eb53b49c960d4ec8644d0bb59b98544054
yousefashrafi <yousef.ashraf@ivas.com.eg>
2020-12-10 11:29:01 +0200

    cron free users

--- app/Console/Commands/Free.php
+++ app/Console/Commands/Free.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\OrangeSubscribe;
 use Illuminate\Console\Command;
+use Illuminate\Http\Request;
 
 class Free extends Command
 {
@@ -31,8 +32,19 @@ class Free extends Command
       $orange_subscribes = OrangeSubscribe::where('subscribe_due_date', date("Y-m-d"))->where('free', 1)->get();
       foreach($orange_subscribes as $subscriber){
         // should make subscription request on orange as free 2 days is ended
+
+        $orangeWeb = new Request;
+        $orangeWeb->service_id = service;
+        $orangeWeb->msisdn = $subscriber->msisdn;
+        $orangeWeb->command = 'Subscribe';
+        $orangeWeb->bearer_type = 'WEB';
+
+        $orange_subscription_code = app('App\Http\Controllers\Api\OrangeApiController')->orangeWeb($orangeWeb);
+
         $subscriber->free = 0;
+        $subscriber->active = $orange_subscription_code;
         $subscriber->save();
+
       }
       return 'done!';
     }
