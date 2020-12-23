1420e418238ac91a35213bc640835d14fbd6b424
emad.mohamed.ivas <emad@ivas.com.eg>
2020-12-23 18:12:43 +0200

    emad,add email test

--- routes/api.php
+++ routes/api.php
@@ -103,7 +103,7 @@ Route::post('web_notify', 'OrangeController@web_notify');
 Route::post('checkStatus', 'Api\OrangeApiController@checkStatus');
 Route::post('orangeWeb', 'Api\OrangeApiController@orangeWeb');
 
-Route::post('testemail', 'Api\OrangeApiController@testemail');
+Route::get('testemail', 'Api\OrangeApiController@testemail');
 
 
 /***************************************/
