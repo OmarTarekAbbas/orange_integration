8c889063960ef74517ca8a4e60192a688b0c5211
emad.mohamed.ivas <emad@ivas.com.eg>
2021-01-20 16:21:28 +0200

    emad,add send today link after charging

--- app/Http/Controllers/OrangeController.php
+++ app/Http/Controllers/OrangeController.php
@@ -768,7 +768,7 @@ var_dump($output) ;
         if( $OrangeSubscribe == 0 ){
           $response_msg = 'تم الاشتراك بنجاح في خدمة اورانج الخير';
           $welcome_message = " لقد تم اشتراكك في خدمة اورنج الخير بنجاح للدخول اضغط علي هذا الرابط";
-          $welcome_message .= "https://orange-elkheer.com/orange_portal_login" ;
+          $welcome_message .= "https://orange-elkheer.com" ;
           $send_message = $welcome_message ;
 
         }elseif($OrangeSubscribe == 1 ){
