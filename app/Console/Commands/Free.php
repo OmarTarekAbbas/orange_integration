e84ff7009104e3536c6fab41c84c1b34864c3fbc
emad.mohamed.ivas <emad@ivas.com.eg>
2021-01-21 11:14:04 +0200

    emad,add expire message

--- app/Console/Commands/Free.php
+++ app/Console/Commands/Free.php
@@ -61,7 +61,7 @@ class Free extends Command
         $subscriber->save();
 
         // send message to the user notify that free trials will end today
-        $expire_message = "سوف تنتهي 3 ايام المجانية اليوم في خدمة اورانج الخير";
+        $expire_message = "باقة أورانج الخير اللي انت مشترك فيها وبتجيلك كل يوم من أورانج هتتجدد ب 1 جنيه فى اليوم، واستهلاك الإنترنت سوف يخصم من الباقة الخاصة بك، ولإلغاء الإشتراك ارسل unsub1 إلى 6124 مجانا.";
         $orange_subscription_code = app('App\Http\Controllers\OrangeController')->sendMessageToUser($subscriber->msisdn,$expire_message);
 
       }
