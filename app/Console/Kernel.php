65525070f52d72ff55dfd3b6bcf011983f377aad
emad.mohamed.ivas <emad@ivas.com.eg>
2021-01-24 17:25:34 +0200

    emad,test cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
         // send today content
        // $schedule->command('subscribe_free')->dailyAt('08:00');
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('08:00');
-       $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('15:33');
+       $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('15:34'); // testing by my number
 
 
         // send  charging to orange
