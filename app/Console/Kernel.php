47626b883cb5eb78a5acdc9769cafd3bbb68051f
emad.mohamed.ivas <emad@ivas.com.eg>
2021-01-24 16:51:59 +0200

    emad,fix orange links

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
         // send today content
        // $schedule->command('subscribe_free')->dailyAt('08:00');
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('08:00');
-       $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('14:58');
+       $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('17:00');
 
 
         // send  charging to orange
