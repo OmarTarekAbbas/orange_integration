33917c776d5aea99a3aba47f7df7ff5de8301219
emad.mohamed.ivas <emad@ivas.com.eg>
2021-01-26 10:13:45 +0200

    emad,edit kernal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,16 +29,17 @@ class Kernel extends ConsoleKernel
 
         // send today content
        // $schedule->command('subscribe_free')->dailyAt('08:00');
-    //   $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('08:00'); // 10 am
-       $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->hourly();
-      //  $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('15:51'); // testing by my number
+       $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('08:00'); // 10 am
+       $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('09:00'); // 11 am
+       $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('10:00'); // 12 pm
+      // $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->hourly();
 
 
         // send  charging to orange
       //  $schedule->command('subscribe_free')->hourly();
-      $schedule->command('subscribe_free')->dailyAt('09:00');  // 11 am
-      $schedule->command('subscribe_free')->dailyAt('10:00');
-      $schedule->command('subscribe_free')->dailyAt('11:00');
+      $schedule->command('subscribe_free')->dailyAt('11:00');  // 1 PM
+      $schedule->command('subscribe_free')->dailyAt('12:00');
+      $schedule->command('subscribe_free')->dailyAt('13:00');
 
 
 
