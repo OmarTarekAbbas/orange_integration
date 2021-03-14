6b07367b6b0eec0baf447621c2e77091a6af72b1
omar tarek <omar.tarek@iVAS.local>
2021-03-14 12:00:48 +0200

    fix csv

--- app/Http/Controllers/AdminOrangeController.php
+++ app/Http/Controllers/AdminOrangeController.php
@@ -521,6 +521,6 @@ class AdminOrangeController extends Controller
               $excel->sheet('Excel', function($sheet) use ($count_user_today, $count_all_active_users, $count_all_unsub_users ,$count_all_pending_users,$count_of_total_free_users,$count_charging_users_not_free,$count_of_all_success_charging,$count_of_all_success_charging_today) {
                   $sheet->loadView('backend.orange.download_excel_orange_statistics')->with("count_user_today", $count_user_today)->with("count_all_active_users", $count_all_active_users)->with("count_all_unsub_users", $count_all_unsub_users)->with("count_all_pending_users", $count_all_pending_users)->with("count_of_total_free_users", $count_of_total_free_users)->with("count_charging_users_not_free",$count_charging_users_not_free)->with("count_of_all_success_charging",$count_of_all_success_charging)->with("count_of_all_success_charging_today",$count_of_all_success_charging_today);
               });
-          })->export('xlsx');
+          })->export('csv');
       }
 }
