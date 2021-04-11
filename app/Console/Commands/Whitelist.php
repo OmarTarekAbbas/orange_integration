e7ad489704931e9acd06c58d424e3d31e901ea9c
mahmoud maher <mahmoud.maher@ivas.com.eg>
2021-04-11 14:20:54 +0200

    update import orange excel

--- app/Console/Commands/Whitelist.php
+++ app/Console/Commands/Whitelist.php
@@ -9,48 +9,47 @@ use App\OrangeWhitelist;
 
 class Whitelist extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'orange_whitelist';
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'orange_whitelist';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Store Orange Whitelist';
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Store Orange Whitelist';
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-      ini_set('memory_limit', '-1');
-      set_time_limit(0);
-      $path= base_path(). "/OrangeWhitelist/Whitelisted_dials.xlsx";
+  /**
+   * Execute the console command.
+   *
+   * @return mixed
+   */
+  public function handle()
+  {
+    ini_set('memory_limit', '-1');
+    set_time_limit(0);
+    $path = base_path() . "/OrangeWhitelist/Whitelisted_dials.xlsx";
 
-      $data = \Excel::load($path)->get();
-
-      if(!empty($data) && $data->count()){
-        foreach ($data as $value) {
-          if (strlen($value->mob)  > 8 ) {
-            $orange_whitelist_item = OrangeWhitelist::where('msisdn', "20".$value->mob)->first();
-            if(!isset($orange_whitelist_item)){
+    \Excel::filter('chunk')->load($path)->chunk(1000, function ($results) {
+      if (!empty($results) && $results->count()) {
+        foreach ($results as $value) {
+          if (strlen($value->mob)  > 8) {
+            $orange_whitelist_item = OrangeWhitelist::where('msisdn', "20" . $value->mob)->first();
+            if (!isset($orange_whitelist_item)) {
               $orange = new OrangeWhitelist;
-              $orange->msisdn = "20".$value->mob;
+              $orange->msisdn = "20" . $value->mob;
               $orange->save();
-            }else{
+            } else {
               $orange = $orange_whitelist_item;
             }
 
 
-            $orange_subscribe_item = OrangeSubscribe::where('msisdn', "20".$value->mob)->first();
-            if(!isset($orange_subscribe_item)){
+            $orange_subscribe_item = OrangeSubscribe::where('msisdn', "20" . $value->mob)->first();
+            if (!isset($orange_subscribe_item)) {
               $orange_subscribe = new OrangeSubscribe;
               $orange_subscribe->msisdn = $orange->msisdn;
               $orange_subscribe->active = 1;  // charging modify
@@ -60,15 +59,14 @@ class Whitelist extends Command
               $orange_subscribe->service_id = productId;
               $orange_subscribe->type = "whitelists";
               $orange_subscribe->save();
-            }else{
+            } else {
               $orange_subscribe_item->orange_channel_id = $orange->id;
               $orange_subscribe_item->save();
             }
-
           }
         }
       }
-      echo "Ok";
-    }
-
+    });
+    echo "Ok";
+  }
 }
