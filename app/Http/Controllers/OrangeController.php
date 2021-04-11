f59ee666afdba4f2720289806f319c29759d7025
mahmoud maher <mahmoud.maher@ivas.com.eg>
2021-04-11 12:43:50 +0200

    update chunk to orange subscribe

--- app/Http/Controllers/OrangeController.php
+++ app/Http/Controllers/OrangeController.php
@@ -1319,21 +1319,23 @@ var_dump($output) ;
     $message = $this->orange_get_today_content();
     $today_message_msisdns = TodayMessage::whereDate('created_at', Carbon::now()->toDateString())->pluck('msisdn');
 
-    OrangeSubscribe::where("active", 1)->whereNotIn('msisdn', $today_message_msisdns)->chunk(1000, function ($orange_subscribe) use ($message) {
-      if ($orange_subscribe->free == 1) {
-        $type = "free";
-      } elseif ($orange_subscribe->active == 1) {
-        $type = "today";
+    OrangeSubscribe::where("active", 1)->whereNotIn('msisdn', $today_message_msisdns)->chunk(1000, function ($orange_subscribes) use ($message) {
+      foreach ($orange_subscribes as $orange_subscribe) {
+        if ($orange_subscribe->free == 1) {
+          $type = "free";
+        } elseif ($orange_subscribe->active == 1) {
+          $type = "today";
+        }
+
+        $this->sendMessageToUser($orange_subscribe->msisdn, $message);
+
+        // add log to DB
+        $TodayMessage  =   new TodayMessage();
+        $TodayMessage->msisdn   = $orange_subscribe->msisdn;
+        $TodayMessage->message   = $message;
+        $TodayMessage->type   =  $type;
+        $TodayMessage->save();
       }
-
-      $this->sendMessageToUser($orange_subscribe->msisdn, $message);
-
-      // add log to DB
-      $TodayMessage  =   new TodayMessage();
-      $TodayMessage->msisdn   = $orange_subscribe->msisdn;
-      $TodayMessage->message   = $message;
-      $TodayMessage->type   =  $type;
-      $TodayMessage->save();
     });
 
     echo "send today content is Done";
@@ -1345,21 +1347,23 @@ var_dump($output) ;
     $message =  "سوف يتم خصم 1 جنيه  فى اليوم، واستهلاك الإنترنت سوف يخصم من الباقة الخاصة بك، ولإلغاء الإشتراك ارسل 0215 إلى 6124 مجانا.";
     $today_message_msisdns = TodayMessage::whereDate('created_at', Carbon::now()->toDateString())->where("type", "=", "charge")->pluck('msisdn');
 
-    OrangeSubscribe::where("active", 1)->where("free", 0)->whereNotIn('msisdn', $today_message_msisdns)->chunk(1000, function ($orange_subscribe) use ($message) {
-      if ($orange_subscribe->free == 1) {
-        $type = "charge";
-      } elseif ($orange_subscribe->active == 1) {
-        $type = "charge";
+    OrangeSubscribe::where("active", 1)->where("free", 0)->whereNotIn('msisdn', $today_message_msisdns)->chunk(1000, function ($orange_subscribes) use ($message) {
+      foreach ($orange_subscribes as $orange_subscribe) {
+        if ($orange_subscribe->free == 1) {
+          $type = "charge";
+        } elseif ($orange_subscribe->active == 1) {
+          $type = "charge";
+        }
+
+        $this->sendMessageToUser($orange_subscribe->msisdn, $message);
+
+        // add log to DB
+        $TodayMessage  =   new TodayMessage();
+        $TodayMessage->msisdn   = $orange_subscribe->msisdn;
+        $TodayMessage->message   = $message;
+        $TodayMessage->type   =  $type;
+        $TodayMessage->save();
       }
-
-      $this->sendMessageToUser($orange_subscribe->msisdn, $message);
-
-      // add log to DB
-      $TodayMessage  =   new TodayMessage();
-      $TodayMessage->msisdn   = $orange_subscribe->msisdn;
-      $TodayMessage->message   = $message;
-      $TodayMessage->type   =  $type;
-      $TodayMessage->save();
     });
 
     echo "send today charging message  is Done";
