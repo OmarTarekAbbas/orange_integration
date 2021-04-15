971baa2923cdf9dc5a8899f3427cfa0d623543b8
emad.mohamed.ivas <emad@ivas.com.eg>
2021-04-15 14:30:23 +0200

    emad,edit

--- app/Constants/OrangeResponseStatus.php
+++ app/Constants/OrangeResponseStatus.php
@@ -4,10 +4,34 @@ namespace App\Constants;
 final class OrangeResponseStatus
 {
   const Success          = 0;
-  const AlreadySuccess   = 1;
+  const AlreadySubscribe   = 1;
   const NotSubscribed    = 2;
   const NotAllowed       = 5;
   const NoBalance        = 6;
   const Technicalproblem = 31;
-}
 
+
+  public static function getList()
+  {
+      return [
+          self::Success           => trans('Success'),
+          self::AlreadySubscribe   => trans('You Are Already Subscribe'),
+          self::NotSubscribed     => trans('You Are Not Subscribe'),
+          self::NotAllowed        => trans('Not Allowed'),
+          self::NoBalance         => trans('No Balance'),
+          self::Technicalproblem  => trans('Technical Problem'),
+      ];
+  }
+
+  /**
+   * Method getLabel
+   *
+   * @param int $key
+   *
+   * @return string
+   */
+  public static function getLabel($key)
+  {
+      return array_key_exists($key, self::getList()) ? self::getList()[$key] : trans("there are error ");
+  }
+}
