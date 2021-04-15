0157621ae18def904a212304b66f9d347f45e0a4
mohamed mahmoud <m.mahmoud@iVAS.local>
2021-04-15 11:53:47 +0200

    make orange flash message

--- resources/views/orange/alerts.blade.php
+++ resources/views/orange/alerts.blade.php
@@ -1,5 +1,5 @@
 @if(session()->has('success'))
-    <div class="alert alert_danger fade show" role="alert">
+    <div class="alert alert-success fade show" role="alert">
         <span class="alert-inner--icon"><i class="ni ni-like-2"></i></span>
         <span class="alert-inner--text"><strong>{{ trans('Success!') }}</strong> {{ session()->get('success') }}</span>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
