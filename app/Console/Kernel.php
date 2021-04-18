c0d612a4697d3ae06bcd6cf48be99e0fe7c17f05
emad.mohamed.ivas <emad@ivas.com.eg>
2021-04-18 09:42:50 +0200

    emad,remove cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,6 +29,7 @@ class Kernel extends ConsoleKernel
 
         // send today content
        // $schedule->command('subscribe_free')->dailyAt('08:00');
+       /*
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('08:00'); // 10 am
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('09:00');
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('10:00'); // 12 pm
@@ -45,8 +46,10 @@ class Kernel extends ConsoleKernel
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('20:00');
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('21:00');
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_today_content')->dailyAt('22:00'); //  10  pm
+       */
 
    // daily deduction message
+   /*
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_daily_deduction')->dailyAt('08:15');
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_daily_deduction')->dailyAt('09:15');
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_daily_deduction')->dailyAt('10:15');
@@ -60,7 +63,7 @@ class Kernel extends ConsoleKernel
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_daily_deduction')->dailyAt('18:15');
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_daily_deduction')->dailyAt('19:15');
        $schedule->call('App\Http\Controllers\OrangeController@orange_send_daily_deduction')->dailyAt('20:15');
-
+*/
 
 
        // weekly deduction message  // weekly at 11 at monday
