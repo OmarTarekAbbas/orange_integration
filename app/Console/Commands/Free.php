20f1a6d14960f8420067c5b1436e370ec103f1d0
emad.mohamed.ivas <emad@ivas.com.eg>
2021-06-07 13:11:46 +0200

    emad,edit

--- app/Console/Commands/Free.php
+++ app/Console/Commands/Free.php
@@ -35,7 +35,7 @@ class Free extends Command
     $email = 'emad@ivas.com.eg';
     $this->sendMail($subject, $email);
 
-    OrangeSubscribe::where('subscribe_due_date', "<=",date("Y-m-d"))->where('free', 1)->chunk(1000, function ($orange_subscribes) {
+    OrangeSubscribe::where('subscribe_due_date', date("Y-m-d"))->where('free', 1)->chunk(1000, function ($orange_subscribes) {
       foreach ($orange_subscribes as $subscriber) {
         // should make subscription request on orange as free 2 days is ended
         $orangeWeb = new Request;
@@ -64,6 +64,38 @@ class Free extends Command
         $orange_subscription_code = app('App\Http\Controllers\OrangeController')->sendMessageToUser($subscriber->msisdn, $expire_message);
       }
     });
+
+
+
+ // handle old
+    OrangeSubscribe::where('subscribe_due_date', "<", date("Y-m-d"))->where('free', 1)->chunk(1000, function ($old_orange_subscribes) {
+      foreach ($old_orange_subscribes as $subscriber) {
+        // should make subscription request on orange as free 2 days is ended
+        $orangeWeb = new Request;
+        $orangeWeb->service_id = productId;
+        $orangeWeb->msisdn = $subscriber->msisdn;
+        $orangeWeb->command = 'SUBSCRIBE';
+        $orangeWeb->bearer_type = 'WEB';
+
+        $orange_subscription_code = app('App\Http\Controllers\Api\OrangeApiController')->orangeWeb($orangeWeb);
+
+        /* =================  Orange status code for sub / unsub api ===================
+        0	success
+        1	already subscribed
+        2	not subscribed
+        5	not allowed
+        6	account problem
+        31	Technical problem
+        */
+
+        $subscriber->free = 0;
+        $subscriber->active = ($orange_subscription_code == "0" || $orange_subscription_code == "1") ? 1 : 0;  // need to be handle  ( 0 or 1  =>active = 1)
+        $subscriber->save();
+
+      }
+    });
+
+
 
     echo 'subscribe_free done!';
   }
