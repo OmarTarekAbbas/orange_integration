4da6a3ec30281cc4f1a8a76a0b9afde310dfdbda
emad.mohamed.ivas <emad@ivas.com.eg>
2021-06-13 17:23:55 +0200

    emad,add life count

--- app/Console/Commands/SubscriberLifeCount.php
+++ app/Console/Commands/SubscriberLifeCount.php
@@ -38,7 +38,7 @@ class SubscriberLifeCount extends Command
      */
     public function handle()
     {
-      OrangeSubscribe::where('active',1)->where('type','!=','whitelists')->chunk(10000, function($subscribers)
+      OrangeSubscribe::where('type','!=','whitelists')->chunk(10000, function($subscribers)
       {
         foreach ($subscribers as $subscriber) {
           $date1 = new \DateTime(date("Y-m-d"));
